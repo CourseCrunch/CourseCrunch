4f5e24b4c38dc5caeae8a9fc8f88e2c0abd6c590
westhav3 <robert.westhaver@mail.utoronto.ca>
2020-02-11 15:05:18 -0500

    Create Postgres user authentication schema and handle registration requests through REST

--- authentication/app.js
+++ authentication/app.js
@@ -2,6 +2,10 @@ var express = require('express');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var registerRouter = require('./routes/register');
+
+var bodyParserURLEncode = require('body-parser').urlencoded({extended:true});
+var path = require('path');
 
 var app = express();
 
@@ -12,5 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/register', registerRouter);
 
 app.listen(3000, () => console.log("App listening on port 3000"));
---
+++ authentication/resources/queries.js
@@ -0,0 +1,59 @@
+const bcrypt = require('bcrypt');
+const uuidv4 = require('uuid/v4');
+const Pool = require('pg').Pool;
+const pool = new Pool({
+	user: 'admin',
+	host: 'localhost',
+	database: 'BBAUTHDB',
+	password: 'Dova64g1s!PenCIl',
+	port: 5432
+})
+
+const checkUserExists = (email, callBack) => {
+	pool.query('SELECT 1 FROM CC_CREDENTIALS WHERE EMAIL = $1', [email], (error, results)  => {
+		if (error) {
+			console.log(error);
+			return callBack('error');
+		} else {
+			return callBack(results.rows);
+		}
+	});
+}
+
+const createUser = (fname, lname, email, program, password, callBack) => {
+	try{
+		bcrypt.hash(password, 10, (bcrypt_err, hash) => {
+			if(bcrypt_err){
+				console.log(bcrypt_err);
+				return callBack("error");
+			} else {
+
+				var uuid = uuidv4();
+				pool.query('INSERT INTO CC_CREDENTIALS (ID, EMAIL, HASH) VALUES ($1, $2, $3)', 
+					[uuid, email, hash], (cred_err, result) => {
+						if(cred_err){
+							console.log(cred_err);
+							return callBack("error");
+						} else {
+							pool.query('INSERT INTO CC_USER_INFO (ID, ROLE, FNAME, LNAME, PROGRAM) VALUES ($1, $2, $3, $4, $5)',
+							[uuid, 'USER', fname, lname, program], (info_err, resultt) => {
+								if(info_err){
+									console.log(info_err);
+								} else {
+									return callBack("User Registered!"); 
+								}
+							});
+						}
+					});
+			}
+		});
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+module.exports = {
+	checkUserExists,
+	createUser
+}
+

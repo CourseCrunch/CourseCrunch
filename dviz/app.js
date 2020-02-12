6c802a1e20072a49eeb6e5fc8d49d2e16141668a
Justin Pham <justinon.pham@mail.utoronto.ca>
2020-02-12 00:07:40 -0500

    BOUN-3 Add ejs templating for data visualization

--- dviz/app.js
+++ dviz/app.js
@@ -1,48 +1,15 @@
 const express = require('express');
+const partials = require('express-partials');
 const path = require('path');
 const bodyparser = require('body-parser')
 const app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use("/public", express.static(__dirname + '/public'));
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-
-app.use('/public', express.static(path.join(__dirname, '/static')));
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, '/static', '/viz.html'));
-});
-
-
-
-app.get('/', (req, res)=>{
-    res.send('Hello World!');
-});
-
-app.get('/tutorial', (req,res)=>{
-    res.send('hitting tutorial route');
-});
-
-app.get('/example/:name/:age', (req, res)=>{
-    console.log(req.params);
-    res.send('example with route params');
-});
-
-app.get('/viz/:porc/:name', (req, res)=>{
-    if (req.params['porc'] == 'cours') {
-        console.log("I want course data");
-        res.send('here is some course data');
-    } else if (req.params['porc'] == 'prof') {
-        console.log("I want prof data");
-        res.send('here is some prof data for');
-    }
-});
-
-// view engine setup
-
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
-
-//app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+app.use('/', indexRouter);
 
 app.listen(3000, () => console.log("App listening on port 3000"));
--- dviz/routes/index.js
+++ dviz/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var course = require('./query/course.js');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({'Hello':'World'});
+router.get('/viz/:porc/:name', function(req, res, next) {
+  if (req.params['porc'] == 'cours') {
+    console.log("I want course data");
+    var q = course(req.params['name']);
+    res.render('pages/index', {dates: q[0], scores: q[1], name: req.params['name']});
+  } else if (req.params['porc'] == 'prof') {
+    console.log("I want prof data");
+    var q = course(req.params['name']);
+    res.render('pages/index', {dates: q[0], scores: q[1], name: req.params['name']});
+  }
 });
 
 module.exports = router;

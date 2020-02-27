0db6d2c36620b808094b58346ebdd5ad11f8cc95
Krish Chowdhary <krish.chowdhary@utoronto.ca>
2020-02-27 12:50:53 -0500

    added fuzzy search and working fuse

--- rmp_api/api.js
+++ rmp_api/api.js
@@ -28,7 +28,6 @@ function number_of_instructors(school_id) {
 
 function query_all_instructors(school_id) {
     var promise = number_of_instructors(school_id).then(instruct_count => {
-        //console.log(instruct_count);
         return Promise.resolve('https://solr-aws-elb-production.ratemyprofessors.com//solr/rmp/select/?solrformat=true&rows='+instruct_count+'&wt=json&q=*%3A*+AND+schoolid_s%3A'+school_id+'&defType=edismax&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=total_number_of_ratings_i+desc&siteName=rmp&start=0&fl=pk_id+teacherfirstname_t+teacherlastname_t+teacherdepartment_s');
     }).then(qs => {
         return axios.get(qs).then(res => {
@@ -43,29 +42,12 @@ function query_all_instructors(school_id) {
 function query_instructor(iid) {
     var query_str = 'https://solr-aws-elb-production.ratemyprofessors.com//solr/rmp/select/?solrformat=true&rows=1&wt=json&defType=edismax&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=total_number_of_ratings_i+desc&siteName=rmp&q=*%3A*+AND+pk_id%3A'+iid+'&start=0&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s+averageeasyscore_rf+averageclarityscore_rf+tag_s_mv+averagehelpfulscore_rf+teacherdepartment_s';
     return axios.get(query_str).then(res => {
-        //console.log(res.data.response.docs.length);
         if (res.data.response.docs.length != 0) return res.data.response.docs[0];
-        //console.log('hi');
         return null;
     }).catch(e => {
-        //console.log('hi');
         return null;
     });
 }
-
-// number_of_instructors('4928').then(res => {
-//     console.log(res);
-// })
-
-// query_all_instructors('4928').then(res => {
-//     console.log(res);
-// });
-
-// query_instructor('1707').then(res => {
-//     console.log(res);
-// }).catch(e => {
-//     console.log(res);
-// })
 
 function get_schools() {
     return {
@@ -88,44 +70,37 @@ function update_instructor_cache() {
                 let doc = mongo.Instruct(row);
                 doc.save((err,result) => {
                     if (err) {
-                        //console.log(err + ' failed');
-                        return;
+                        return null;
                     }
-                    //console.log(row['fullname']+ ' saved');
                 });
             }
         }).catch(e => {
-            console.log(e);
             return;
         })
     }
 }
 
-let fuse_global = {};
+let fuse_global = null;
 
 function get_fuse() {
-    return mongo.Instruct.find().then(res => {
-        if (fuse_global == null) {
+    if (fuse_global == null) {
+        return mongo.Instruct.find().then(res => {
             fuse_global = new Fuse(res, options);
-            fuse_global['uuid'] = Math.random();
-        }
-        return fuse_global;
-    }).catch(e => {
-        return {};
-    });
+            fuse_global['uid'] = Math.random();
+            return fuse_global;
+        }).catch(e => {
+            return null;
+        });
+    } else {
+        return Promise.resolve(fuse_global);
+    }
 }
-
-// mongo.Instruct.find((err,res) => {
-//     console.log(fuse.search('Bailey Lee'));
-// })
 
 function fuzzy_search(instructor_name) {
     return get_fuse().then(f => {
         return f.search(instructor_name);
     })
 }
-
-//update_instructor_cache();
 
 fuzzy_search('Bailey Lee').then(res => {
     console.log(res[0]);
@@ -143,5 +118,6 @@ module.exports = {
     find_instructor: query_instructor,
     find_all_instructors: query_all_instructors,
     all_schools: get_schools,
-    init: update_instructor_cache
+    init: update_instructor_cache,
+    find_instructor: fuzzy_search
 }

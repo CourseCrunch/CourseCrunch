766923e017c624be553a1702118235aec0d7cf67
Liam Aiello <liamstabsyou99@gmail.com>
2020-02-28 13:36:44 -0500

    Fix old routes to adhere to the linter

--- authentication/routes/login.js
+++ authentication/routes/login.js
@@ -1,69 +1,52 @@
-var express = require('express');
-var router = express.Router();
-var app = express();
-var dbReq = require('../resources/queries');
-var bodyParser = require('body-parser');
-var validator = require('validator');
+/* eslint-disable no-useless-catch */
+const express = require('express');
 
-const emptyString = function(input) { if(input == 'undefined') {return ''} else {return input}}
+const router = express.Router();
+const bodyParser = require('body-parser');
+const validator = require('validator');
+const dbReq = require('../resources/queries');
+
+const emptyString = function (input) { if (input === 'undefined') { return ''; } return input; };
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
-	extended: true
+    extended: true,
 }));
 
-router.get('/', function(req, res) {
-    try{
-        
-        const {email, password} = req.body;
+router.get('/', (req, res) => {
+    try {
+        const { email, password } = req.body;
 
-        //Sanitize like the Romans
-        const san_email = emptyString(validator.trim(validator.escape(validator.normalizeEmail(email + ''))));
-        const san_password = emptyString(validator.trim(validator.escape(password + '')));
+        // Sanitize like the Romans
+        const sanEmail = emptyString(validator.trim(validator.escape(validator.normalizeEmail(`${email}`))));
+        const sanPassword = emptyString(validator.trim(validator.escape(`${password}`)));
 
-        if(!(validator.isEmail(san_email)) || !(san_email.length < 200)){
-			res.status(406).send('Please enter a valid email.');
-        }else {
-
-            dbReq.checkUserExists(san_email, function(response){
-                if(response.length == 0){
-
-                    res.status(406).send("There is no account associated with this email.");
-
-                }else{
-
-                    try{
-
-                        dbReq.validatePW(san_email, san_password, function(resp){
-                            if(resp == "Invalid Password"){
-
-                                res.status(406).send("Invalid Password for the given email.");
-
-                            }else if(resp == "error"){
-
-                                res.status(500).send("Error during password validation.");
-
-                            }else{
-                            
-                            //Sessions and other concepts here?
-                                res.status(200).send("Successful login");
+        if (!(validator.isEmail(sanEmail)) || !(sanEmail.length < 200)) {
+            res.status(406).send('Please enter a valid email.');
+        } else {
+            dbReq.checkUserExists(sanEmail, (response) => {
+                if (response.length === 0) {
+                    res.status(406).send('There is no account associated with this email.');
+                } else {
+                    try {
+                        dbReq.validatePW(sanEmail, sanPassword, (resp) => {
+                            if (resp === 'Invalid Password') {
+                                res.status(406).send('Invalid Password for the given email.');
+                            } else if (resp === 'error') {
+                                res.status(500).send('Error during password validation.');
+                            } else {
+                            // Sessions and other concepts here?
+                                res.status(200).send('Successful login');
                             }
-                         });
-
-                    }catch(e) {
-
+                        });
+                    } catch (e) {
                         res.status(500);
-                        console.log(e);
-
                     }
                 }
             });
         }
-
-    }catch(e){  
-
-        throw(e)
-
+    } catch (e) {
+        throw (e);
     }
 });
 

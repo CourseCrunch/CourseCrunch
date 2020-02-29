c27c659b2109a8de0036c5694698e4e288481d51
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2020-02-29 17:43:34 -0500

    BOUN-26 Clean up code with Lint
    Clean up code according to Lint Standards

--- front_end/components/NavBar/NavBar.js
+++ front_end/components/NavBar/NavBar.js
@@ -2,31 +2,31 @@ import React from 'react';
 import Link from 'next/link';
 import './NavBar.css';
 
-function Greeting(props){
-    const isLoggedIn = props.isLoggedIn;
-    if (!isLoggedIn){
-        return <li><label>Login</label></li>
+function Greeting(props) {
+    const { isLoggedIn } = props;
+    if (!isLoggedIn) {
+        return <li><label>Login</label></li>;
     }
     return <><li><label>User Profile</label></li>
-        <li><label>Logout</label></li></>
+        <li><label>Logout</label></li></>;
 }
-class NavBar extends React.Component{
-    render(){
-      return  <header className ="NavBar">
-        <nav className = "NavBar_navigation">
-            <div></div>
-            <div className = "NavBar_logo"><Link href = '/'><a>THE LOGO</a></Link></div>
-            <div className = "spacer"/>
-            <div className = "NavBar_navigation-items">
-                <ul>
-                    <li><label>CGPA Calculator</label></li>
-                    <li><label>Course Review</label></li>
-                    <li><Link href = "/recommendations"><a>Course Recommendation</a></Link></li>
-                    {Greeting(this.props)}
-                </ul>
-            </div>
-        </nav>
-    </header>
+class NavBar extends React.Component {
+    render() {
+        return <header className ="NavBar">
+            <nav className = "NavBar_navigation">
+                <div></div>
+                <div className = "NavBar_logo"><Link href = '/'><a>THE LOGO</a></Link></div>
+                <div className = "spacer"/>
+                <div className = "NavBar_navigation-items">
+                    <ul>
+                        <li><label>CGPA Calculator</label></li>
+                        <li><label>Course Review</label></li>
+                        <li><Link href = "/recommendations"><a>Course Recommendation</a></Link></li>
+                        {Greeting(this.props)}
+                    </ul>
+                </div>
+            </nav>
+        </header>;
     }
 }
 export default NavBar;
--- front_end/pages/index.js
+++ front_end/pages/index.js
@@ -1,23 +1,24 @@
 import NavBar from '../components/NavBar/NavBar';
 import './index.css';
 import './hover.css';
+
 const Index = () => (
-  <div>
-  <NavBar isLoggedIn = {false}/>
-  <div className = "container">
-    <div className = "back"></div>
-    <h1>Course Crunch</h1>
-    <div className = "buttonPanel">
-    <ul className="buttonList">
-      <li><button className ="hvr-grow">Course Recommendations</button></li>
-      <li><button className ="hvr-grow">PLACE HOLDER</button></li>
-      <li><button className ="hvr-grow">PLACE HOLDER</button></li>
-      <li><button className ="hvr-grow"> PLACE HOLDER</button></li>
-      <li><button className ="hvr-grow">PLACE HOLDER</button></li>
-    </ul>
+    <div>
+        <NavBar isLoggedIn = {false}/>
+        <div className = "container">
+            <div className = "back"></div>
+            <h1>Course Crunch</h1>
+            <div className = "buttonPanel">
+                <ul className="buttonList">
+                    <li><button className ="hvr-grow">Course Recommendations</button></li>
+                    <li><button className ="hvr-grow">PLACE HOLDER</button></li>
+                    <li><button className ="hvr-grow">PLACE HOLDER</button></li>
+                    <li><button className ="hvr-grow"> PLACE HOLDER</button></li>
+                    <li><button className ="hvr-grow">PLACE HOLDER</button></li>
+                </ul>
+            </div>
+        </div>
     </div>
-  </div>
-  </div>
 );
 
 export default Index;

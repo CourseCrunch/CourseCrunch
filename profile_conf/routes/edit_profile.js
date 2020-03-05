f314f86c69c5efd88bbe2946319f4958df438848
westhav3 <robert.westhaver@mail.utoronto.ca>
2020-03-04 20:43:16 -0500

    Change edit_profile to take in user info and construct query based on changed info

--- profile_conf/routes/edit_profile.js
+++ profile_conf/routes/edit_profile.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const validator = require('validator');
 const dbReq = require('../resources/queries');
+const utils = require('../resources/utils');
 
 // this line is borrowed from creator of validator as work-around for validating alphas with spaces
 const valid = (input) => input.split(' ').every((s) => validator.isAlpha(s));
@@ -27,12 +28,12 @@ router.get('/', (req, res, next) => {
 router.patch('/', (req, res) => {
     try {
         // retrieve info from request
-        const { fname, lname, program } = req.body;
+        const { unsanFname, unsanLname, unsanProgram } = req.body;
 
         // sanitize input
-        const sanFname = emptyString(validator.trim(`${fname}`));
-        const sanLname = emptyString(validator.trim(`${lname}`));
-        const sanProgram = emptyString(validator.trim(`${program}`));
+        const sanFname = emptyString(validator.trim(`${unsanFname}`));
+        const sanLname = emptyString(validator.trim(`${unsanLname}`));
+        const sanProgram = emptyString(validator.trim(`${unsanProgram}`));
 
         // TEMPORARY UNTIL PROPER SESSION AND SESSION SERVER IS SET UP FROM LOGIN
         const uuid = req.cookies.sessionid;
@@ -51,28 +52,47 @@ router.patch('/', (req, res) => {
             res.status(406).send('please enter a valid program');
         } else {
             // If the any of the fields are not empty, update them
-            if (!(validator.isEmpty(sanFname))) {
-                const promiseUpdateFname = dbReq.updateUserFname(uuid, sanFname);
-                promiseUpdateFname.catch((updateFnameError) => {
-                    console.log(updateFnameError);
-                    res.status(500).send('Sorry an error occurred while processing your request');
-                });
-            }
-            if (!(validator.isEmpty(sanLname))) {
-                const promiseUpdateLname = dbReq.updateUserLname(uuid, sanLname);
-                promiseUpdateLname.catch((updateLnameError) => {
-                    console.log(updateLnameError);
-                    res.status(500).send('Sorry an error occurred while processing your request');
-                });
-            }
-            if (!(validator.isEmpty(sanProgram))) {
-                const promiseUpdateProgram = dbReq.updateUserProgram(uuid, sanProgram);
-                promiseUpdateProgram.catch((updateProgramError) => {
-                    console.log(updateProgramError);
-                    res.status(500).send('Sorry an error occurred while processing your request');
-                });
-            }
-            res.status(200).send('Profile Updated!');
+            const promiseGetUserInfo = dbReq.getUserInfo(uuid);
+            promiseGetUserInfo.then((queryResult) => {
+                const { fname, lname, program } = queryResult.rows[0];
+                const columnList = [];
+                const paramList = [];
+                if ((!(validator.isEmpty(sanFname))) && fname !== sanFname) {
+                    columnList.push('FNAME');
+                    paramList.push(sanFname);
+                }
+
+                if ((!(validator.isEmpty(sanLname))) && lname !== sanLname) {
+                    columnList.push('LNAME');
+                    paramList.push(sanLname);
+                }
+
+                if ((!(validator.isEmpty(sanProgram))) && program !== sanProgram) {
+                    columnList.push('PROGRAM');
+                    paramList.push(sanProgram);
+                }
+
+                if (columnList.length > 0) {
+                    const promiseConstructQuery = utils.constructQuery(columnList, paramList, uuid);
+                    promiseConstructQuery.then((query) => {
+                        const promiseUpdateUser = dbReq.updateUser(query);
+                        promiseUpdateUser.then(() => {
+                            res.status(200).send('Profile Updated!');
+                        }).catch((queryError) => {
+                            console.log(queryError);
+                            res.status(500).send('Sorry an error occurred while processing your request');
+                        });
+                    }).catch((constructError) => {
+                        console.log(constructError);
+                        res.status(500).send('Sorry an error occurred while processing your request');
+                    });
+                } else {
+                    res.status(200).send('No Changes Made');
+                }
+            }).catch((userInfoError) => {
+                console.log(userInfoError);
+                res.status(500).send('Sorry an error occurred while processing your request');
+            });
         }
     } catch (e) {
         console.log(e);

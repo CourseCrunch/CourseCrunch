10ca0672a084bbc568bcb02a974a55c24bf5486a
Naaz <sibianjali@gmail.com>
2020-03-09 21:51:31 -0400

    Add queries for waitlists

--- waitlists/emails/queries.js
+++ waitlists/emails/queries.js
@@ -9,31 +9,47 @@ const pool = new Pool({
 });
 
 function getWaitlists(){
-    pool.query('SELECT HASH FROM CC_CREDENTIALS WHERE EMAIL = $1', [email], (error, results) => {
+    var waitlists = new Object(); 
+    pool.query('SELECT * FROM CC_USER_WAITLIST', [], (error, results) => {
         if (error) {
             console.log(error);
             return callBack('error');
         }else {
-            console.log(results.rows[0]);
-            var pw_hash = results.rows[0].HASH;
-            
-            bcrypt.compare(password, pw_hash, (valid_err, hash) => {
-                if (valid_err){
+            console.log(results);
+            results.forEach(result => {
+                var course = result.CourseCode;
+                var instructor = result.Instructor;
+                var userID = result.ID;
+                var term = result.Term;
+                // waitlists will look like: 
+                // {"CSC209": {"Furkan Alaca": {"W": ["naaz.sibia@utoronto.ca", ...]}}}
+                if(!waitlists.hasOwnProperty(course))
+                    waitlist[course] = new Object();
 
-                    console.log(valid_err);
-                    return callBack("error");
-
-                }else if(!hash){
-                    
-                    return callBack("Invalid Password.");
-
-                }else if(hash){
-
-                    return callBack("Valid Password.");
-
+                if(!waitlists[course].hasOwnProperty(instructor)){
+                    waitlist[course][instructor] = new Object();
                 }
+
+                if(!waitlists[course][instructor].hasOwnProperty(term)){
+                    waitlist[course][instructor][term] = [];
+                }
+                var email = getUserEmail(userID);
+                if(email) waitlist[course][instructor][term].push(email);
             });
         }
     });
+}
 
+function getUserEmail(userID){
+    pool.query('SELECT EMAIL FROM CC_CREDENTIALS WHERE ID=$1', [userID], (error, results) => {
+        if (error) {
+            console.log(error);
+            return callBack('error');
+        }else {
+            if(length(results) == 0){
+                return null;
+            }
+            return results[0].email;
+        }
+    });
 }

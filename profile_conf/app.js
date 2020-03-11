9130469d3000fea95d297eb917fe3ed7262b5278
westhav3 <robert.westhaver@mail.utoronto.ca>
2020-03-11 06:49:09 -0400

    BOUN-12 prepare profile_conf backend to connect with frontend

--- profile_conf/app.js
+++ profile_conf/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const cors = require('cors');
 const editProfRouter = require('./routes/edit_profile');
 const changeEmailRouter = require('./routes/change_email');
 const changePassRouter = require('./routes/change_pass');
@@ -11,9 +11,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cors());
 app.use('/edit_profile', editProfRouter);
 app.use('/change_email', changeEmailRouter);
 app.use('/change_password', changePassRouter);
 
-app.listen(3000, () => console.log('App listening on port 3000'));
+app.listen(3008, () => console.log('App listening on port 3008'));
--- profile_conf/routes/edit_profile.js
+++ profile_conf/routes/edit_profile.js
@@ -21,82 +21,106 @@ router.use(bodyParser.urlencoded({
 
 router.use(cookieParser());
 
-router.get('/', (req, res) => {
-    res.json({ info: 'Temp Profile conf page' });
+router.post('/', (req, res) => {
+    try {
+        // retrieve uuid from request
+        const { unsanUuid } = req.body;
+
+        const uuid = emptyString(validator.trim(validator.escape(`${unsanUuid}`)));
+
+        if (uuid === '') {
+            res.status(400).send();
+        } else {
+            const promiseGetUserInfo = dbReq.getUserInfo(uuid);
+            promiseGetUserInfo.then((queryResult) => {
+                const { fname, lname, program } = queryResult.rows[0];
+                res.status(200).json({ fNam: fname, lNam: lname, prog: program });
+            }).catch((constructError) => {
+                console.log(constructError);
+                res.status(500).send();
+            });
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).send();
+    }
 });
 
 router.patch('/', (req, res) => {
     try {
         // retrieve info from request
-        const { unsanFname, unsanLname, unsanProgram } = req.body;
+        const {
+            unsanUuid, unsanFname, unsanLname, unsanProgram,
+        } = req.body;
 
         // sanitize input
-        const sanFname = emptyString(validator.trim(`${unsanFname}`));
-        const sanLname = emptyString(validator.trim(`${unsanLname}`));
-        const sanProgram = emptyString(validator.trim(`${unsanProgram}`));
+        const sanFname = emptyString(validator.trim(validator.escape(`${unsanFname}`)));
+        const sanLname = emptyString(validator.trim(validator.escape(`${unsanLname}`)));
+        const sanProgram = emptyString(validator.trim(validator.escape(`${unsanProgram}`)));
+        const uuid = emptyString(validator.trim(validator.escape(`${unsanUuid}`)));
 
-        // TEMPORARY UNTIL PROPER SESSION AND SESSION SERVER IS SET UP FROM LOGIN
-        const uuid = req.cookies.sessionid;
-
-        // NOTE: all res status messages are for debugging and presenting to our TA,
-        //       will remove when front end is built
-        // validate that all sanitized inputs follow DB constraints
-        if (!((valid(sanFname) || valid2(sanFname) || validator.isEmpty(sanFname))
-                && sanFname.length < 26)) {
-            res.status(406).send('Please enter a valid first name');
-        } else if (!((valid(sanLname) || valid2(sanLname) || validator.isEmpty(sanLname))
-                && sanLname.length < 41)) {
-            res.status(406).send('Please enter a valid last name');
-        } else if (!((valid(sanProgram) || valid2(sanProgram) || validator.isEmpty(sanProgram))
-                && sanProgram.length < 256)) {
-            res.status(406).send('please enter a valid program');
+        if (uuid === '') {
+            res.status(400).send();
         } else {
-            // If the any of the fields are not empty, update them
-            const promiseGetUserInfo = dbReq.getUserInfo(uuid);
-            promiseGetUserInfo.then((queryResult) => {
-                const { fname, lname, program } = queryResult.rows[0];
-                const columnList = [];
-                const paramList = [];
-                if ((!(validator.isEmpty(sanFname))) && fname !== sanFname) {
-                    columnList.push('FNAME');
-                    paramList.push(sanFname);
-                }
+            // validate that all sanitized inputs follow DB constraints
+            if (!((valid(sanFname) || valid2(sanFname) || validator.isEmpty(sanFname))
+                    && sanFname.length < 26)) {
+                res.status(406).send();
+            } else if (!((valid(sanLname) || valid2(sanLname) || validator.isEmpty(sanLname))
+                    && sanLname.length < 41)) {
+                res.status(406).send();
+            } else if (!((valid(sanProgram) || valid2(sanProgram) || validator.isEmpty(sanProgram))
+                    && sanProgram.length < 256)) {
+                res.status(406).send();
+            } else {
+                // If the any of the fields are not empty, update them
+                const promiseGetUserInfo = dbReq.getUserInfo(uuid);
+                promiseGetUserInfo.then((queryResult) => {
+                    const { fname, lname, program } = queryResult.rows[0];
+                    const columnList = [];
+                    const paramList = [];
+                    if ((!(validator.isEmpty(sanFname))) && fname !== sanFname) {
+                        columnList.push('FNAME');
+                        paramList.push(sanFname);
+                    }
 
-                if ((!(validator.isEmpty(sanLname))) && lname !== sanLname) {
-                    columnList.push('LNAME');
-                    paramList.push(sanLname);
-                }
+                    if ((!(validator.isEmpty(sanLname))) && lname !== sanLname) {
+                        columnList.push('LNAME');
+                        paramList.push(sanLname);
+                    }
 
-                if ((!(validator.isEmpty(sanProgram))) && program !== sanProgram) {
-                    columnList.push('PROGRAM');
-                    paramList.push(sanProgram);
-                }
+                    if ((!(validator.isEmpty(sanProgram))) && program !== sanProgram) {
+                        columnList.push('PROGRAM');
+                        paramList.push(sanProgram);
+                    }
 
-                if (columnList.length > 0) {
-                    const promiseConstructQuery = utils.constructQuery(columnList, paramList, uuid);
-                    promiseConstructQuery.then((query) => {
-                        const promiseUpdateUser = dbReq.updateUser(query);
-                        promiseUpdateUser.then(() => {
-                            res.status(200).send('Profile Updated!');
-                        }).catch((queryError) => {
-                            console.log(queryError);
-                            res.status(500).send('Sorry an error occurred while processing your request');
+                    if (columnList.length > 0) {
+                        const promiseConstructQuery = 
+				    utils.constructQuery(columnList, paramList, uuid);
+                        promiseConstructQuery.then((query) => {
+                            const promiseUpdateUser = dbReq.updateUser(query);
+                            promiseUpdateUser.then(() => {
+                                res.status(200).send();
+                            }).catch((queryError) => {
+                                console.log(queryError);
+                                res.status(500).send();
+                            });
+                        }).catch((constructError) => {
+                            console.log(constructError);
+                            res.status(500).send();
                         });
-                    }).catch((constructError) => {
-                        console.log(constructError);
-                        res.status(500).send('Sorry an error occurred while processing your request');
-                    });
-                } else {
-                    res.status(200).send('No Changes Made');
-                }
-            }).catch((userInfoError) => {
-                console.log(userInfoError);
-                res.status(500).send('Sorry an error occurred while processing your request');
-            });
+                    } else {
+                        res.status(200).send();
+                    }
+                }).catch((userInfoError) => {
+                    console.log(userInfoError);
+                    res.status(500).send();
+                });
+            }
         }
     } catch (e) {
         console.log(e);
-        res.status(500);
+        res.status(500).send();
     }
 });
 

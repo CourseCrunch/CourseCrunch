eec70b9d41b02ee347f2504f04a5c3dc520caeea
Krish Chowdhary <krish.chowdhary@utoronto.ca>
2020-04-01 21:22:50 -0400

    fixed hover issue

--- front_end/pages/index.js
+++ front_end/pages/index.js
@@ -1,13 +1,13 @@
-import Link from 'next/link';
+import React from 'react';
 import Head from 'next/head';
-import NavBar from '../components/NavBar/NavBar';
+import { Button } from 'semantic-ui-react';
 import Router from 'next/router';
+import NavBar from '../components/NavBar/NavBar';
 
 import './index.css';
 import './hover.css';
 
 class Index extends React.Component {
-
     constructor(props) {
         super(props);
         if (typeof window !== 'undefined') {
@@ -18,25 +18,28 @@ class Index extends React.Component {
     }
 
 
+    // eslint-disable-next-line class-methods-use-this
     render() {
-        return (
-            <div>
-                <NavBar isLoggedIn = {true}/>
-                <div className = "container">
-                    <div className = "back"></div>
-                    <h1>Course Crunch</h1>
-                    <div className = "buttonPanel">
-                        <ul className="buttonList">
-                            <li><button className ="hvr-grow">Course Recommendations</button></li>
-                            <li><button className ="hvr-grow">PLACE HOLDER</button></li>
-                            <li><button className ="hvr-grow">PLACE HOLDER</button></li>
-                            <li><button className ="hvr-grow"> PLACE HOLDER</button></li>
-                            <li><button className ="hvr-grow">PLACE HOLDER</button></li>
-                        </ul>
-                    </div>
+        return (<div>
+            <Head>
+                <style>{'body,html { height:100% }'}</style>
+                <link href="https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css" rel="stylesheet" key="test"/>
+            </Head>
+            <NavBar isLoggedIn = {true}/>
+            <div className = "container">
+                <div className = "back"></div>
+                <h1>Course Crunch</h1>
+                <div className = "buttonPanel">
+                    <ul className="buttonList">
+                        <li><a href='/recommendations'><Button className ="hvr-grow">Course Recommendations</Button></a></li>
+                        <li><a href="#"><Button className ="hvr-grow">PLACE HOLDER</Button></a></li>
+                        <li><a href="/instructor"><Button className ="hvr-grow">View Instructors</Button></a></li>
+                        <li><Button className ="hvr-grow"> PLACE HOLDER</Button></li>
+                        <li><Button className ="hvr-grow">PLACE HOLDER</Button></li>
+                    </ul>
                 </div>
             </div>
-        );
+        </div>);
     }
 }
 

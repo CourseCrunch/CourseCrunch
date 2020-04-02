af317d2116f5027de2ab1d5f648015a15c60fe34
Krish Chowdhary <krish.chowdhary@utoronto.ca>
2020-04-02 17:40:37 -0400

    more cleaning

--- waitlists/app.js
+++ waitlists/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -11,5 +10,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.listen(process.env.WAITLISTPORT, () => console.log(`App listening on port ${process.env.WAITLISTPORT}`));
